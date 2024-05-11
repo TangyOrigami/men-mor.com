889960db5b256de7c55725b332f0173adf934ce5
TangyOrigami <csaenz613@gmail.com>
2024-05-11 14:04:32 -0500

    fixed tailwind install

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./*.html'],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
